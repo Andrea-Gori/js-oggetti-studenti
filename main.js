06eac0b0a9cff3b9c7c5664eddd1be790380360f
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-03-16 17:39:01 +0100

    completo

--- main.js
+++ main.js
@@ -14,26 +14,55 @@ $(document).ready(function () {
   /* Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto. */
 
   for (var key in studente) {
-    document.getElementById('dati-studente').innerHTML += key + ': ' + sudente[key] + '<br>';
+    document.getElementById('dati-studente').innerHTML += key + ': ' + studente[key] + '<br>';
     console.log(key, studente[key]);
   }
 
+  /* Creare un array di oggetti di studenti */
 
+  var studenti = [
+    {
+      nome: 'Caio',
+      cognome: 'Sempronio',
+      età: 'troppi da contare',
+    },
+    {
+      nome: 'Mario',
+      cognome: 'Rossi',
+      età: '38',
+    },
+    {
+      nome: 'Kekko',
+      cognome: 'Sama',
+      età: '19',
+    }
+  ];
+  console.log(studenti);
 
+  /* Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
 
+  for (var i = 0; i < studenti.length; i++) {
+    $('#dati-studenti').append(studenti[i].nome + ' ' + studenti[i].cognome + '<br>');
+  }
 
+  /* Dare la possibilità all’utente di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
+  $('#invio').click(function () {
+    var nome = $('#nome').val();
+    var cognome = $('#cognome').val();
+    var eta = $('#eta').val();
 
+    studente = {
+      nome: nome,
+      cognome: cognome,
+      età: eta,
+    }
 
+    studenti.push(studente)
+    console.log(studenti);
 
+    $('#nuovo-studente').append(studenti[studenti.length -1].nome + ' ' + studenti[studenti.length -1].cognome + ' ' + studenti[studenti.length -1].età + '<br>');
 
-
-
-
-
-
-
-
-
+  })
 })
 
